a60d206114c1daffa33a957816d390ca6cebb59e
Tamil Selvan <tsx113@gmail.com>
2019-12-10 18:35:23 +1300

    Create download_blob.ps1

---
+++ powershell/azure/download_blob.ps1
@@ -0,0 +1,28 @@
+$LogTime = Get-Date -Format "yyyy-MM-dd"
+$LogFile = 'C:\Log\' + "Download_Blob_" + $LogTime + ".log"
+
+Start-Transcript $LogFile -Append
+$Hour = 1
+$BackupTime=(Get-Date).AddHours(- $Hour)
+$containername=''
+$storage_account = ''
+$storage_account_key =''
+$storage_context = New-AzureStorageContext -StorageAccountName $storage_account -StorageAccountKey $storage_account_key -ErrorAction Stop -Verbose
+$contaninerlist = Get-AzureStorageContainer -Context $storage_context -Verbose | where-object name -eq $containername| Select-Object name
+$contaninerlist | ForEach-Object {
+  $containername = $_.Name
+  $filelist = Get-AzureStorageBlob -Context $storage_context -Container $containername -Blob "*.trn" | Where-Object LastModified -GT $BackupTime | Select-Object *
+  foreach ($file in $filelist)
+  {
+    if ($file.Name -ne $null)
+    {
+
+      "Backups Since : " + $Hour + " hours - " + $BackupTime + " | " + $storage_account + "\" + $containername + "\" + $file.Name + " | Last Modified : " + $file.LastModified + " | Backup Start : " + $file.StartDate
+        Get-AzureStorageBlobContent -Context $storage_context -Container $containername -Blob $file.Name -Destination  "\\SQLDBANET\blob\" #-WhatIf
+      $flag = 1
+    }
+  }
+  if ($flag -eq 0 -or $flag -eq $null) { Write-Host "No Recent backups:",$BackupTime}
+}
+
+Stop-Transcript
