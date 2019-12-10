97202d5efe38775f56824458af3f6fcf0c507b13
Tamil Selvan <tsx113@gmail.com>
2019-12-10 18:42:17 +1300

    Update get_remote_lastboottime.ps1

--- powershell/get_remote_lastboottime.ps1
+++ powershell/get_remote_lastboottime.ps1
@@ -1,8 +1,8 @@
 $servers= Get-Content "C:\SQLDBA\server.txt"
 
-$ps_version = ForEach-Object {
+$ps_Scriptblock = ForEach-Object {
 
 Invoke-Command  -Computername $servers -Scriptblock {Get-WmiObject win32_operatingsystem | select csname, @{LABEL=’LastBootUpTime’;EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}} -Verbose
 }
 
-$ps_version | Out-GridView
+$ps_Scriptblock | Out-GridView
