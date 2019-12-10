6c03387679b0d51310c3c7f825f737f4106b2740
Tamil Selvan <tsx113@gmail.com>
2019-12-10 13:39:42 +1300

    Update and rename get_AlwaysonAvailabilityGroups_ConnectionDetails.sql to get_mssqlserver_Alwayson_ConnectionDetails.sql

---
+++ get_mssqlserver_Alwayson_ConnectionDetails.sql
@@ -0,0 +1,34 @@
+WITH AG
+AS (
+	SELECT es.login_name
+		,es.program_name
+		,db_name(es.database_id) [db_name]
+		,ec.client_net_address
+		,ec.client_tcp_port
+		,agl.dns_name
+		,aglip.ip_address
+		,agl.port
+		,ec.encrypt_option
+	FROM sys.availability_group_listeners agl
+	INNER JOIN sys.availability_group_listener_ip_addresses aglip ON agl.listener_id = aglip.listener_id
+	INNER JOIN sys.dm_exec_connections ec ON ec.local_net_address = aglip.ip_address
+	INNER JOIN sys.dm_exec_sessions es ON ec.session_id = es.session_id
+	
+	UNION ALL
+	
+	SELECT es.login_name
+		,es.program_name
+		,db_name(es.database_id) [db_name]
+		,ec.client_net_address
+		,ec.client_tcp_port
+		,@@SERVERNAME AS [dns_name]
+		,sr.value_data AS [ip_Address]
+		,ec.local_tcp_port AS [port]
+		,ec.encrypt_option
+	FROM sys.dm_server_registry sr
+	INNER JOIN sys.dm_exec_connections ec ON sr.value_name = 'IpAddress'
+		AND ec.local_net_address = sr.value_data
+	INNER JOIN sys.dm_exec_sessions es ON ec.session_id = es.session_id
+	)
+SELECT *
+FROM AG
