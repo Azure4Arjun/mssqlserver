36c01506e6a815e220a4a7212847ba6c73af0b5b
Tamil Selvan <tsx113@gmail.com>
2019-12-10 15:10:38 +1300

    Create get_mssqlserver_db_log_file_space_details.sql

---
+++ get_mssqlserver_db_log_file_space_details.sql
@@ -0,0 +1,26 @@
+CREATE TABLE #logsize (
+	Dbname SYSNAME
+	,Log_File_Size_MB DECIMAL(38, 2) DEFAULT(0)
+	,log_Space_Used_MB DECIMAL(30, 2) DEFAULT(0)
+	,log_Free_Space_MB DECIMAL(30, 2) DEFAULT(0)
+	)
+GO
+
+INSERT INTO #logsize (
+	Dbname
+	,Log_File_Size_MB
+	,log_Space_Used_MB
+	,log_Free_Space_MB
+	)
+EXEC sp_msforeachdb 'use [?]; 
+  select DB_NAME() AS DbName, 
+sum(size)/128.0 AS Log_File_Size_MB, 
+sum(CAST(FILEPROPERTY(name, ''SpaceUsed'') AS INT))/128.0 as log_Space_Used_MB, 
+SUM( size)/128.0 - sum(CAST(FILEPROPERTY(name,''SpaceUsed'') AS INT))/128.0 AS log_Free_Space_MB  
+from sys.database_files  where type=1 group by type'
+
+SELECT @@SERVERNAME [Server],*
+	,Cast((log_Free_Space_MB / Log_File_Size_MB) * 100 AS DECIMAL(10, 2)) [%Free]
+FROM #logsize
+
+DROP TABLE #logsize
