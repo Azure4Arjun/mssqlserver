d70ba08d289582ab57d01c4be065e0f7fc3270b1
Tamil Selvan <tselvan@sqldba.net>
2020-03-12 12:55:21 +1300

    Add user_name

--- get_database_last_backup_details.sql
+++ get_database_last_backup_details.sql
@@ -2,10 +2,13 @@ WITH backupset
 AS (
 	SELECT bs.database_name
 		,bs.type bstype
+		,user_name
 		,MAX(backup_finish_date) MAXbackup_finish_date
+		
 	FROM msdb.dbo.backupset bs
 	GROUP BY bs.database_name
 		,bs.type
+		,user_name
 	)
 	,main
 AS (
@@ -19,6 +22,7 @@ AS (
 		,CAST(bs.backup_size / 1024 / 1024 / 1024 AS DECIMAL(10, 4)) backup_size_GB
 		,CAST(bs.compressed_backup_size / 1024 / 1024 / 1024 AS DECIMAL(10, 4)) compressed_backup_size_GB
 		,bmf.physical_device_name
+		,bss.user_name
 	FROM master.sys.databases db
 	LEFT OUTER JOIN backupset bss ON bss.database_name = db.name
 	LEFT OUTER JOIN msdb.dbo.backupset bs ON bs.database_name = db.name
@@ -37,8 +41,10 @@ SELECT @@servername [Server]
 	,backup_size_GB
 	,compressed_backup_size_GB
 	,physical_device_name
+	,user_name
 FROM sys.databases sd
 LEFT JOIN main m ON sd.name = m.name
+where type='D' and user_name='DCS\S-MicrosoftSQl'
 ORDER BY name
 	,type
 	,backup_finish_date
