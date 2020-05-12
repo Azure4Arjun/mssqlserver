8a85b2518804eec09a048054792bcd2abd8eff82
Tamil Selvan <tselvan@sqldba.net>
2020-05-12 21:05:10 +1200

    Update set_db_newfilepath_wip.sql

--- set_db_newfilepath_wip.sql
+++ set_db_newfilepath_wip.sql
@@ -1,16 +1,16 @@
 SET NOCOUNT ON;
 
-declare @data_folder varchar(max) = 'd:\mssql\data\';
-declare @log_folder varchar(max) = 'e:\mssql\data\';
-declare @tempdb_folder varchar(max) = 't:\mssql\data\';
+declare @data_folder varchar(max) = 'd:\mssql\data\'
+declare @log_folder varchar(max) = 'e:\mssql\data\'
+declare @tempdb_folder varchar(max) = 't:\mssql\data\'
 
-create table #script_out ( script_order int, dbname varchar(max) , script  text )
+create table #script_out ( script_order int, dbname varchar(max) ,file_type int, script  text )
 
-insert into #script_out ( script_order , dbname  , script  )
-select '0' ,null,'/*modify database file location*/'
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '0' ,'_',0,'/*modify database file location*/'
 
-insert into #script_out ( script_order , dbname  , script  )
-select '1' script_order , db_name(mf.database_id) dbname, 'use master' + CHAR(13)+CHAR(10) +'go'+ CHAR(13)+CHAR(10) +'alter database ' + db_name(mf.database_id) + ' modify file (name = ''' + name + ''', filename = ''' + case 
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '1' script_order , db_name(mf.database_id) dbname,type, 'use master' + CHAR(13)+CHAR(10) +'go'+ CHAR(13)+CHAR(10) +'alter database ' + db_name(mf.database_id) + ' modify file (name = ''' + name + ''', filename = ''' + case 
 		when type = 0 and db_name(mf.database_id) !='tempdb'
 			then @data_folder
 		when type = 1 and db_name(mf.database_id) !='tempdb'
@@ -26,11 +26,11 @@ where db_name(mf.database_id) not in (
 		)
 order by database_id , type asc
 
-insert into #script_out ( script_order , dbname  , script  )
-select '0' ,null,'/*set database offline*/'
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '2' ,'_',0,'/*set database offline*/'
 
-insert into #script_out ( script_order , dbname  , script  )
-select '2' script_order, name dbname , 'use master' + CHAR(13)+CHAR(10) +'alter database ['+name+'] set offline with rollback immediate;' script from sys.databases
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '3' script_order, name dbname ,0, 'use master' + CHAR(13)+CHAR(10) +'alter database ['+name+'] set offline with rollback immediate;' script from sys.databases
 where db_name(database_id) not in (
 		'master'
 		,'model'
@@ -38,11 +38,11 @@ where db_name(database_id) not in (
 		)
 order by database_id 
 
-insert into #script_out ( script_order , dbname  , script  )
-select '0' ,null,'/*move files to data & log folders '+ CHAR(13)+CHAR(10) +'set database online once files have been moved*/'
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '4' ,'_',0,'/*move files to data & log folders '+ CHAR(13)+CHAR(10) +'set database online once files have been moved*/'
 
-insert into #script_out ( script_order , dbname  , script  )
-select '3' script_order, name dbname , 'use master' + CHAR(13)+CHAR(10) +'go'+ CHAR(13)+CHAR(10) +'alter database ['+name+'] set online;' script from sys.databases
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '5' script_order, name dbname ,0, 'use master' + CHAR(13)+CHAR(10) +'go'+ CHAR(13)+CHAR(10) +'alter database ['+name+'] set online;' script from sys.databases
 where db_name(database_id) not in (
 		'master'
 		,'model'
@@ -51,4 +51,5 @@ where db_name(database_id) not in (
 order by database_id 
 
 select script from #script_out
+order by script_order,dbname,file_type
 drop table #script_out
