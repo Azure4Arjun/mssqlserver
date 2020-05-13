bf240f06ddff121293a9a8c3f783f323818145a2
Tamil Selvan <tselvan@sqldba.net>
2020-05-13 12:03:35 +1200

    Update set_db_newfilepath_wip.sql

--- set_db_newfilepath_wip.sql
+++ set_db_newfilepath_wip.sql
@@ -53,3 +53,54 @@ order by database_id
 select script from #script_out
 order by script_order,dbname,file_type
 drop table #script_out
+
+-----------ROLLBACK--------
+
+SET NOCOUNT ON;
+
+declare @data_folder varchar(max) = 'd:\mssql\data\'
+declare @log_folder varchar(max) = 'e:\mssql\data\'
+declare @tempdb_folder varchar(max) = 't:\mssql\data\'
+
+create table #script_out ( script_order int, dbname varchar(max) ,file_type int, script  text )
+
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '0' ,'_',0,'/*modify database file location*/'
+
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '1' script_order , db_name(mf.database_id) dbname,type, 'use master' + CHAR(13)+CHAR(10) +'go'+ CHAR(13)+CHAR(10) +'alter database ' + db_name(mf.database_id) + ' modify file (name = ''' + name + ''', filename = ''' + physical_name+''')' + CHAR(13)+CHAR(10) +'go' script
+from sys.master_files mf
+where db_name(mf.database_id) not in (
+		'master'
+		,'model'
+		,'msdb'
+		)
+order by database_id , type asc
+
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '2' ,'_',0,'/*set database offline*/'
+
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '3' script_order, name dbname ,0, 'use master' + CHAR(13)+CHAR(10) +'alter database ['+name+'] set offline with rollback immediate;' script from sys.databases
+where db_name(database_id) not in (
+		'master'
+		,'model'
+		,'msdb','tempdb'
+		)
+order by database_id 
+
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '4' ,'_',0,'/*move files to data & log folders '+ CHAR(13)+CHAR(10) +'set database online once files have been moved*/'
+
+insert into #script_out ( script_order , dbname  ,file_type, script  )
+select '5' script_order, name dbname ,0, 'use master' + CHAR(13)+CHAR(10) +'go'+ CHAR(13)+CHAR(10) +'alter database ['+name+'] set online;' script from sys.databases
+where db_name(database_id) not in (
+		'master'
+		,'model'
+		,'msdb','tempdb'
+		)
+order by database_id 
+
+select script from #script_out
+order by script_order,dbname,file_type
+drop table #script_out
