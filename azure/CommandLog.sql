5fd52ca9cc9311768927e6ff34b203aef4494ec2
Tamil Selvan <tsx113@gmail.com>
2020-05-29 15:20:53 +1200

    Add files via upload

---
+++ azure/CommandLog.sql
@@ -0,0 +1,36 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT  schema_name FROM    information_schema.schemata WHERE   schema_name = 'az_dba' ) 
+BEGIN
+	EXEC dbo.sp_executesql @command= N'CREATE SCHEMA [az_dba]'
+END
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[az_dba].[CommandLog]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [az_dba].[CommandLog](
+  [ID] [int] IDENTITY(1,1) NOT NULL,
+  [DatabaseName] [sysname] NULL,
+  [SchemaName] [sysname] NULL,
+  [ObjectName] [sysname] NULL,
+  [ObjectType] [char](2) NULL,
+  [IndexName] [sysname] NULL,
+  [IndexType] [tinyint] NULL,
+  [StatisticsName] [sysname] NULL,
+  [PartitionNumber] [int] NULL,
+  [ExtendedInfo] [xml] NULL,
+  [Command] [nvarchar](max) NOT NULL,
+  [CommandType] [nvarchar](60) NOT NULL,
+  [StartTime] [datetime2](7) NOT NULL,
+  [EndTime] [datetime2](7) NULL,
+  [ErrorNumber] [int] NULL,
+  [ErrorMessage] [nvarchar](max) NULL,
+ CONSTRAINT [PK_CommandLog] PRIMARY KEY CLUSTERED
+(
+  [ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+END
+GO
+
