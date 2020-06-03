0e9b1fc12d8afe056b35bfe01eeccb724678c9e4
Tamil Selvan <tselvan@sqldba.net>
2020-06-03 12:57:13 +1200

    Update exec_sql_IndexOptimize.sql

--- azure/maintenance/exec_sql_IndexOptimize.sql
+++ azure/maintenance/exec_sql_IndexOptimize.sql
@@ -1,8 +1,4 @@
 DECLARE @sql_db VARCHAR(max)
 SET @sql_db = ( SELECT DB_NAME(db_id()))
 
-EXECUTE [sqldba].[IndexOptimize] @Databases = sql_db
-	,@UpdateStatistics = 'ALL'
-	,@OnlyModifiedStatistics = 'Y'
-	,@StatisticsSample = 100
-	,@LogToTable = 'Y'
+EXECUTE [sqldba].[IndexOptimize] @Databases = @sql_db
