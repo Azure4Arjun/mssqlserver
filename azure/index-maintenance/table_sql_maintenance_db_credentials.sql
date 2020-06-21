812f1c991f5084b1ac691437b1ed9f6ea97bd73f
Tamil Selvan <tselvan@sqldba.net>
2020-06-22 07:57:41 +1200

    Update table_sql_maintenance_db_credentials.sql

--- azure/index-maintenance/table_sql_maintenance_db_credentials.sql
+++ azure/index-maintenance/table_sql_maintenance_db_credentials.sql
@@ -1,4 +1,4 @@
-CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'xxxxxxxxxxxxxxxx';
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = '';
 GO
 
 CREATE CERTIFICATE azure_maintenance_cert  
@@ -10,10 +10,14 @@ CREATE SYMMETRIC KEY azure_maintenance_Key
     ENCRYPTION BY CERTIFICATE azure_maintenance_cert ;  
 GO  
 
+//DROP TABLE [sqldba].[sql_maintenance_db_credentials] 
+
 IF NOT EXISTS (SELECT  schema_name FROM    information_schema.schemata WHERE   schema_name = 'sqldba' ) 
 BEGIN
 	EXEC dbo.sp_executesql @command= N'CREATE SCHEMA [sqldba]'
 END
+												   
+   
 
 CREATE TABLE [sqldba].[sql_maintenance_db_credentials] (
 	[ID] [int] IDENTITY(1,1) NOT NULL,
@@ -25,10 +29,10 @@ CREATE TABLE [sqldba].[sql_maintenance_db_credentials] (
 	[comments] NVARCHAR(max) NULL
 	)
 
-
 -- Open the symmetric key with which to encrypt the data.  
 OPEN SYMMETRIC KEY azure_maintenance_Key 
    DECRYPTION BY CERTIFICATE azure_maintenance_cert;  
+
 
 INSERT INTO [sqldba].[sql_maintenance_db_credentials]
            ([customer]
@@ -38,10 +42,10 @@ INSERT INTO [sqldba].[sql_maintenance_db_credentials]
            ,[created]
            ,[comments])
      VALUES
-           ('XXXX'
-		   , EncryptByKey( Key_GUID('azure_maintenance_Key'), CONVERT(nvarchar,'xxxxxxxxxxxxxxxx') ) 
-           , EncryptByKey( Key_GUID('azure_maintenance_Key'), CONVERT(nvarchar,'xxxxxxxxxxxxxxxx') ) 
- 	       , EncryptByKey( Key_GUID('azure_maintenance_Key'), CONVERT(nvarchar,'xxxxxxxxxxxxxxxx') ) 
+           ('DCCUST'
+		   , EncryptByKey( Key_GUID('azure_maintenance_Key'), CONVERT(nvarchar(max),'') ) 
+           , EncryptByKey( Key_GUID('azure_maintenance_Key'), CONVERT(nvarchar(max),'') ) 
+ 	       , EncryptByKey( Key_GUID('azure_maintenance_Key'), CONVERT(nvarchar(max),'') ) 
            ,getdate()
            ,'NA')
 
@@ -52,17 +56,29 @@ OPEN SYMMETRIC KEY azure_maintenance_Key
    DECRYPTION BY CERTIFICATE azure_maintenance_cert;  
 
 
+
 SELECT [ID]
       ,[customer]
-	  ,CONVERT(nvarchar,DecryptByKey([login])) [login]
-	  ,CONVERT(nvarchar,DecryptByKey([password])) [password]
-	  ,CONVERT(nvarchar,DecryptByKey([tenant])) [tenant]
+	  ,CONVERT(nvarchar(max),DecryptByKey([login])) [login]
+	  ,CONVERT(nvarchar(max),DecryptByKey([password])) [password]
+	  ,CONVERT(nvarchar(max),DecryptByKey([tenant])) [tenant]
       ,[created]
       ,[comments]
   FROM [sqldba].[sql_maintenance_db_credentials]
 
+CLOSE SYMMETRIC KEY azure_maintenance_Key;
+GO
+
+OPEN SYMMETRIC KEY azure_maintenance_Key DECRYPTION BY CERTIFICATE azure_maintenance_cert;
+
+SELECT vsmp.[customer] 
+	,[Server_Name]
+	,[Databases]
+	,[Script]
+	,CONVERT(nvarchar(max), DecryptByKey([login])) [login]
+	,CONVERT(nvarchar(max), DecryptByKey([password])) [password]
+	,CONVERT(nvarchar(max), DecryptByKey([tenant])) [tenant]
+FROM [sqldba].[vw_sql_maintenance_parameters] vsmp
+LEFT JOIN [sqldba].[sql_maintenance_db_credentials] smdc ON vsmp.[customer] = smdc.[customer];
 
 CLOSE SYMMETRIC KEY azure_maintenance_Key;
-
-
-	
