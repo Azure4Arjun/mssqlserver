fa9e24c47373c50d2f63afa44e629e42c912dd09
Tamil Selvan <tselvan@sqldba.net>
2020-08-16 22:57:16 +1200

    SpaceInGB

--- general/get_mssqlserver_data_log_drive_space_details.sql
+++ general/get_mssqlserver_data_log_drive_space_details.sql
@@ -3,13 +3,13 @@ SELECT DISTINCT DB_NAME(dovs.database_id) DBName
 	,mf.physical_name PhysicalFileLocation
 	,dovs.logical_volume_name AS LogicalName
 	,dovs.volume_mount_point AS Drive
-	,CONVERT(INT, dovs.total_bytes / 1048576.0) / 1024 AS AvilableSpaceInMB
-	,CONVERT(INT, dovs.available_bytes / 1048576.0) / 1024 AS FreeSpaceInMB
+	,CONVERT(INT, dovs.total_bytes / 1048576.0) / 1024 AS AvilableSpaceInGB
+	,CONVERT(INT, dovs.available_bytes / 1048576.0) / 1024 AS FreeSpaceInGB
 	,CAST((CONVERT(FLOAT, dovs.available_bytes / 1048576.0) / 1024) / (CONVERT(FLOAT, dovs.total_bytes / 1048576.0) / 1024) * 100 AS DECIMAL(10, 2)) AS [%_Free]
 FROM sys.master_files mf
 CROSS APPLY sys.dm_os_volume_stats(mf.database_id, mf.FILE_ID) dovs
 --WHERE type_desc = 'LOG'
-ORDER BY FreeSpaceInMB ASC
+ORDER BY FreeSpaceInGB DESC
 GO
 
 
